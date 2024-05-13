6ae3a36b4b0af44183d9fe640edf6dd91dc15bd0
Edson Junior <soneddjr@gmail.com>
2024-05-13 14:16:08 +0200

    create cards for the blogposts

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,40 @@
 import Link from 'next/link';
 import { blogPosts } from '../services/blogPosts';
+import { Card, CardBody, Heading, Stack } from '@chakra-ui/react';
+import Image from 'next/image';
 
 export default async function Home() {
   const { data } = await blogPosts();
 
   return (
-    <>
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
       {!data?.length ? (
         <p>there are no blogposts</p>
       ) : (
         data?.map((post) => {
+          const featuredImage = post?.attributes.featuredImage.data?.attributes;
+
           return (
-            <Link href={`/${post?.attributes?.slug}`} key={post.id}>
-              {post?.attributes?.title}
-            </Link>
+            <>
+              <Card marginBottom="8" key={post.attributes.title}>
+                <Link href={`/${post?.attributes?.slug}`} key={post.id}>
+                  <CardBody>
+                    <Image
+                      src={featuredImage?.url}
+                      width={featuredImage?.width}
+                      height={featuredImage?.height}
+                      alt={featuredImage?.alternativeText || ''}
+                    />
+                    <Stack mt="6" spacing="3">
+                      <Heading size="md">{post?.attributes?.title}</Heading>
+                    </Stack>
+                  </CardBody>
+                </Link>
+              </Card>
+            </>
           );
         })
       )}
-    </>
+    </div>
   );
 }

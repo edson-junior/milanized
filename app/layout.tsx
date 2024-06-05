b05d8b0d35d778733696953e13fc8a8fd8f16bb1
Edson Junior <soneddjr@gmail.com>
2024-06-05 14:32:38 +0200

    remove manually added metatags in favor of seo strapi component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from 'next';
+import type { Viewport } from 'next';
 import { Open_Sans } from 'next/font/google';
 import '../styles/globals.css';
 import Header from '../components/Header';
@@ -6,20 +6,6 @@ import Footer from '../components/Footer';
 import Providers from '../components/Providers';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
-
-// TODO: make the following metadata variables dynamic
-
-const title = 'Milanized!';
-const description =
-  'Milanized is an English-language website for internationals in Italy. Visit us for news, culture, history, and hotspots.';
-
-export const metadata: Metadata = {
-  title,
-  description,
-  alternates: {
-    canonical: `${process.env.NEXT_PUBLIC_STRAPI_CLIENT_URL}`
-  }
-};
 
 export const viewport: Viewport = {
   themeColor: 'system'

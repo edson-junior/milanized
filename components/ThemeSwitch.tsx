84cae3c95965e09c598f7b5472ec116d13b2f4c5
Edson Junior <soneddjr@gmail.com>
2024-06-07 15:01:58 +0200

    add aria label to theme switch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
+import { Moon, Sun } from 'lucide-react';
 
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false);
@@ -13,11 +14,25 @@ export default function ThemeSwitch() {
     return;
   }
 
+  const toggleTheme = (theme = '') => {
+    return function () {
+      setTheme(theme);
+    };
+  };
+
   if (resolvedTheme === 'dark') {
-    return <button onClick={() => setTheme('light')}>Light</button>;
+    return (
+      <button onClick={toggleTheme('light')} aria-label="switch to light theme">
+        <Sun className="text-yellow-400" />
+      </button>
+    );
   }
 
   if (resolvedTheme === 'light') {
-    return <button onClick={() => setTheme('dark')}>Dark</button>;
+    return (
+      <button onClick={toggleTheme('dark')} aria-label="switch to dark theme">
+        <Moon className="text-gray-400" />
+      </button>
+    );
   }
 }

b68c2b0a10c669613040bf93ef19b06bc8c5bd07
Edson Junior <soneddjr@gmail.com>
2024-06-24 23:30:21 +0200

    add ignoreBrowserTokenWarning to sanity config

--- client.ts
+++ client.ts
@@ -5,5 +5,6 @@ export default createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
-  useCdn: true
+  useCdn: true,
+  ignoreBrowserTokenWarning: true
 });

4bd51badbaceb7b99c2dee99212392fe825a54e4
Edson Junior <soneddjr@gmail.com>
2024-06-25 09:11:41 +0200

    remove old privacy policy content

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,12 +1,3 @@
-import { promises as fs } from 'fs';
-
-import parse from 'html-react-parser';
-
 export default async function PrivacyPolicy() {
-  const html = await fs.readFile(
-    process.cwd() + '/app/privacy-policy/privacy-policy.html',
-    'utf8'
-  );
-
-  return <>{parse(html)}</>;
+  return <>privacy policy</>;
 }

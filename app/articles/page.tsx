63c72ef47ac54f1212a342001f189452513743af
Edson Junior <soneddjr@gmail.com>
2024-06-26 10:40:36 +0200

    create articles page

---
+++ app/articles/page.tsx
@@ -0,0 +1,119 @@
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+import Heading from '@/components/ui/heading';
+import imageUrlBuilder from '@sanity/image-url';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import client from '@/client';
+import { Blog, Page } from '@/sanity.types';
+
+function urlFor(source: SanityImageSource) {
+  return imageUrlBuilder(client).image(source);
+}
+
+export async function generateMetadata() {
+  const homepage = await getPage();
+
+  if (homepage) {
+    const metaData: Metadata = {
+      title: homepage.metadata?.title,
+      description: homepage.metadata?.description,
+      alternates: {
+        canonical: `${process.env.NEXT_PUBLIC_CLIENT_URL}`
+      }
+    };
+
+    return metaData;
+  }
+}
+
+export default async function Articles() {
+  const posts = await getPosts();
+  const homepage = await getPage();
+
+  if (homepage) {
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-6">
+        <h1 className="absolute left-[-999em]">{`${homepage.metadata?.title} - ${homepage.title}`}</h1>
+        {!posts?.length ? (
+          <p>there are no blogposts</p>
+        ) : (
+          posts?.map(({ _id, metadata, title, summary, featuredImage }) => {
+            return (
+              <Link
+                href={`/${metadata?.slug}`}
+                className="group shadow-md rounded-sm overflow-hidden border border-border"
+                key={_id}
+              >
+                {featuredImage && (
+                  <Image
+                    width="250"
+                    height="250"
+                    src={urlFor(featuredImage).width(600).url()}
+                    alt={featuredImage.alt || ''}
+                    loading="eager"
+                    priority
+                    className="block w-full object-cover h-52"
+                  />
+                )}
+
+                <div className="p-4 pb-6">
+                  <Heading className="text-xl block mb-4 group-hover:text-blue-700">
+                    {title}
+                  </Heading>
+
+                  <p className="text text-sm line-clamp-4 align-baseline">
+                    {summary}
+                  </p>
+                </div>
+              </Link>
+            );
+          })
+        )}
+      </div>
+    );
+  }
+}
+
+async function getPage(): Promise<Page | undefined> {
+  const query = `*[_type == 'page' && metadata.slug.current == 'homepage'][0] {
+    _id,
+    title,
+    metadata {
+      'slug': slug.current,
+      title,
+      noIndex,
+      image,
+      description
+    }
+  }`;
+
+  try {
+    const data = await client.fetch(query);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getPosts(): Promise<Blog[] | undefined> {
+  const query = `*[_type == 'blog' && !(_id in path('drafts.**'))] {
+    _id,
+    title,
+    summary,
+    content,
+    featuredImage,
+    metadata {
+      'slug': slug.current
+    }
+  }`;
+
+  try {
+    const data = await client.fetch(query);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,19 @@ export default function Header() {
   return (
     <header className="bg-black">
       <div className="max-w-7xl	mx-auto p-4 flex">
-        <Link href="/" className="text-white font-bold uppercase text-2xl">
+        <Link
+          href="/"
+          className="text-white font-bold uppercase text-lg lg:text-2xl"
+        >
           Milanized!
         </Link>
 
-        <nav className="text-white flex mx-4">
+        <nav className="text-white flex ml-auto">
           <ul className="flex items-center gap-2">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/articles">Articles</Link>
             </li>
-            <li className="group flex flex-row relative">
+            {/* <li className="group flex flex-row relative">
               Life in Italy
               <ul className="hidden group-hover:block absolute left-0 top-full bg-black p-6 min-w-60">
                 <li>
@@ -22,7 +25,7 @@ export default function Header() {
                   </Link>
                 </li>
               </ul>
-            </li>
+            </li> */}
             <li>
               <Link href="/about">About</Link>
             </li>

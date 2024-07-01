eddbdbb298590ddae7dfb2278cafb4defa628f5e
Edson Junior <soneddjr@gmail.com>
2024-07-01 12:16:27 +0200

    add consent options to consent banner

--- components/ConsentBanner.tsx
+++ components/ConsentBanner.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // import Script from 'next/script';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button } from './ui/button';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
@@ -11,6 +11,10 @@ export default function ConsentBanner() {
   const [consentMode, setConsentMode] = useState(false);
   const pathname = usePathname();
 
+  const refAnalytics = useRef<HTMLInputElement>(null);
+  const refPreferences = useRef<HTMLInputElement>(null);
+  const refMarketing = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     const value = localStorage?.getItem('consentMode');
 
@@ -19,137 +23,116 @@ export default function ConsentBanner() {
 
   if (consentMode) {
     return (
-      <>
-        {/* <Script id="manage-consent-cookie">
-          {`
-            <script>
-              function hideBanner() {
-                  document.getElementById('cookie-consent-banner').style.display = 'none';
-              }
-
-
-                  document.getElementById('btn-accept-all').addEventListener('click', function() {
-
-                      hideBanner();
-                  });
-                  document.getElementById('btn-accept-some').addEventListener('click', function() {
-                      setConsent({
-                          necessary: true,
-                          analytics: document.getElementById('consent-analytics').checked,
-                          preferences: document.getElementById('consent-preferences').checked,
-                          marketing: document.getElementById('consent-marketing').checked
-                      });
-                      hideBanner();
-                  });
-                  document.getElementById('btn-reject-all').addEventListener('click', function() {
-                      setConsent({
-                          necessary: false,
-                          analytics: false,
-                          preferences: false,
-                          marketing: false
-                      });
-                      hideBanner();
-                  });
-
-
-          </script>
-
-          `}
-        </Script> */}
-        <div className="p-4 fixed left-0 right-0 bottom-0 text-sm">
-          <div
-            id="cookie-consent-banner"
-            className={`p-4 bg-white border-solid border rounded border-border flex flex-col w-full ${isBannerHidden || pathname === '/privacy-policy' ? 'hidden' : ''}`}
-          >
-            <div className="mb-4">
-              <h3 className="font-bold mb-2">Cookie settings</h3>
-              <p>
-                We use cookies to deliver the best possible user experience.
-              </p>
-            </div>
-            <div className="flex gap-2 items-center">
-              <Button
-                id="btn-accept-all"
-                className="cookie-consent-button btn-success"
-                onClick={() => {
+      <div className="p-4 fixed left-0 right-0 bottom-0 text-sm">
+        <div
+          id="cookie-consent-banner"
+          className={`p-4 bg-white border-solid border rounded border-border flex flex-col w-full sm:w-auto ${isBannerHidden || pathname === '/privacy-policy' ? 'hidden' : ''}`}
+        >
+          <div className="mb-4">
+            <h3 className="font-bold mb-2">Cookie settings</h3>
+            <p>We use cookies to deliver the best possible user experience.</p>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-4 mb-4">
+            <label className="flex gap-1 cursor-pointer items-center">
+              <input
+                id="consent-necessary"
+                type="checkbox"
+                value="Necessary"
+                disabled
+              />
+              Necessary
+            </label>
+            <label className="flex gap-1 cursor-pointer items-center">
+              <input
+                id="consent-analytics"
+                type="checkbox"
+                value="Analytics"
+                ref={refAnalytics}
+              />
+              Analytics
+            </label>
+            <label className="flex gap-1 cursor-pointer items-center">
+              <input
+                id="consent-preferences"
+                type="checkbox"
+                value="Preferences"
+                ref={refPreferences}
+              />
+              Preferences
+            </label>
+            <label className="flex gap-1 cursor-pointer items-center">
+              <input
+                id="consent-marketing"
+                type="checkbox"
+                value="Marketing"
+                ref={refMarketing}
+              />
+              Marketing
+            </label>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 items-center">
+            <Button
+              id="btn-accept-all"
+              className="cookie-consent-button btn-success"
+              onClick={() => {
+                setConsent({
+                  necessary: true,
+                  analytics: true,
+                  preferences: true,
+                  marketing: true
+                });
+                setIsBannerHidden(true);
+              }}
+            >
+              Accept All
+            </Button>
+            <Button
+              id="btn-accept-some"
+              className="cookie-consent-button btn-outline"
+              onClick={() => {
+                if (
+                  refAnalytics.current &&
+                  refPreferences.current &&
+                  refMarketing.current
+                ) {
                   setConsent({
                     necessary: true,
-                    analytics: true,
-                    preferences: true,
-                    marketing: true
+                    analytics: refAnalytics.current?.checked,
+                    preferences: refPreferences.current?.checked,
+                    marketing: refMarketing.current?.checked
                   });
+
                   setIsBannerHidden(true);
-                }}
-              >
-                Accept All
-              </Button>
-              {/* <Button
-            id="btn-accept-some"
-            className="cookie-consent-button btn-outline"
-          >
-            Accept Selection
-          </Button> */}
-              <Button
-                id="btn-reject-all"
-                className="cookie-consent-button btn-grayscale"
-                onClick={() => {
-                  setConsent({
-                    necessary: false,
-                    analytics: false,
-                    preferences: false,
-                    marketing: false
-                  });
-                  setIsBannerHidden(true);
-                }}
-              >
-                Deny
-              </Button>
-              <Link
-                href="/privacy-policy"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Privacy Policy
-              </Link>
-              <div className="cookie-consent-options hidden">
-                <label>
-                  <input
-                    id="consent-necessary"
-                    type="checkbox"
-                    value="Necessary"
-                    disabled
-                  />
-                  Necessary
-                </label>
-                <label>
-                  <input
-                    id="consent-analytics"
-                    type="checkbox"
-                    value="Analytics"
-                  />
-                  Analytics
-                </label>
-                <label>
-                  <input
-                    id="consent-preferences"
-                    type="checkbox"
-                    value="Preferences"
-                  />
-                  Preferences
-                </label>
-                <label>
-                  <input
-                    id="consent-marketing"
-                    type="checkbox"
-                    value="Marketing"
-                  />
-                  Marketing
-                </label>
-              </div>
-            </div>
+                }
+              }}
+            >
+              Accept Selection
+            </Button>
+            <Button
+              id="btn-reject-all"
+              className="cookie-consent-button btn-grayscale"
+              onClick={() => {
+                setConsent({
+                  necessary: false,
+                  analytics: false,
+                  preferences: false,
+                  marketing: false
+                });
+                setIsBannerHidden(true);
+              }}
+            >
+              Deny All
+            </Button>
+            <Link
+              href="/privacy-policy"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Privacy Policy
+            </Link>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }

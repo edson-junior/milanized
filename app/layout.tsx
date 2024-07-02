815b3c6178716da9704ac6f59f06b57a28f12fd1
Edson Junior <soneddjr@gmail.com>
2024-07-02 22:58:12 +0200

    add google tag manager through @next/third-parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,7 @@ import '../styles/globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Providers from '../components/Providers';
-import Script from 'next/script';
-import parse from 'html-react-parser';
-import ConsentBanner from '@/components/ConsentBanner';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -21,61 +19,14 @@ type RootLayoutProps = Readonly<{
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en-GB" suppressHydrationWarning>
-      <Script id="consent-mode-setup">
-        {parse(`
-          window.dataLayer = window.dataLayer || [];
-
-          function gtag() {
-            dataLayer.push(arguments);
-          }
-
-          if (localStorage.getItem('consentMode') === null) {
-            gtag('consent', 'default', {
-              'ad_storage': 'denied',
-              'analytics_storage': 'denied',
-              'personalization_storage': 'denied',
-              'functionality_storage': 'denied',
-              'security_storage': 'denied',
-            });
-          } else {
-            gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')));
-          }
-
-          if (localStorage.getItem('userId') != null) {
-              window.dataLayer.push({
-                'user_id': localStorage.getItem('userId')
-              });
-          }
-        `)}
-      </Script>
-
-      {/* Google Tag Manager */}
-      <Script id="google-analytics">
-        {parse(`(function(w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({
-                  'gtm.start': new Date().getTime(),
-                  event: 'gtm.js'
-              });
-              var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s),
-                  dl = l != 'dataLayer' ? '&l=' + l : '';
-              j.async = true;
-              j.src =
-                  'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-              f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', '${process.env.GTM_ID}');`)}
-      </Script>
-      {/* End Google Tag Manager */}
-
       <body className={`${openSans.className} bg-black`}>
         <Providers>
-          {/* <div className="min-h-screen flex flex-col justify-between"> */}
           <Header />
           <div className="flex-grow bg-white pb-20">{children}</div>
           <Footer />
-          <ConsentBanner />
-          {/* </div> */}
+          {/* ${process.env.NEXT_PUBLIC_GTM_ID} */}
+          {/* <ConsentBanner /> */}
+          <GoogleTagManager gtmId={`${process.env.NEXT_PUBLIC_GTM_ID}`} />
         </Providers>
       </body>
     </html>

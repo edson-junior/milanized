d9c9567604cefaaa04412010234f2a0a3c7072a9
Edson Junior <soneddjr@gmail.com>
2024-07-05 15:34:08 +0200

    fix sanity dataset environment variable

--- client.ts
+++ client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 
 export default createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   token: process.env.SANITY_API_TOKEN,
   useCdn: true,

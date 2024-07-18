f2f3b5cb30b51fee6b400dddf365d8d9761e13ae
Edson Junior <soneddjr@gmail.com>
2024-07-18 12:54:29 +0200

    fix privacy policy page

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,19 +1,51 @@
+import type { Metadata } from 'next';
 import client from '@/client';
 import BlockRendererClient from '@/components/BlockRenderClient';
 import Heading from '@/components/ui/heading';
 import { Page } from '@/sanity.types';
+
+export async function generateMetadata() {
+  const homepage = await getPage();
+
+  if (homepage) {
+    const metaData: Metadata = {
+      title: homepage.metadata?.title,
+      description: homepage.metadata?.description,
+      alternates: {
+        canonical: `${process.env.CLIENT_URL}/privacy-policy`
+      }
+    };
+
+    return metaData;
+  }
+
+  return {};
+}
 
 export default async function PrivacyPolicy() {
   const data = await getPage();
 
   if (data) {
     return (
-      <div>
-        <Heading as="h1" className="text-2xl lg:text-5xl">
-          {data.title}
-        </Heading>
-        {data.content && <BlockRendererClient value={data.content} />}
-      </div>
+      <>
+        <div
+          className={`group block shadow-md mb-6 relative bg-[url(/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fqua4h5qu%2Fproduction%2F2d2b8abbcbce0ee0c69e4ef6a411fa4d92ec9a09-2048x1152.jpg%3Fw%3D1280&w=3840&q=75)] bg-no-repeat w-full h-52 md:h-80 bg-cover bg-center bg-blend-darken before:block before:w-full before:h-full before:absolute before:backdrop-blur-sm before:bg-black/70`}
+        >
+          {/* TODO:
+
+    fix hero image on every single page, add hero image to contact
+    page as well, each with it's own background image */}
+          <div className="text-white flex align-middle h-full flex-col justify-center max-w-7xl mx-auto px-4 py-4 relative">
+            <Heading as="h1" className="text-2xl lg:text-5xl">
+              {data.title}
+            </Heading>
+            <p className="leading-7 mb-8">{`Last Updated: 18/07/2024`}</p>
+          </div>
+        </div>
+        <div className="max-w-7xl mx-auto px-4 py-4">
+          {data.content && <BlockRendererClient value={data.content} />}
+        </div>
+      </>
     );
   }
 }
@@ -24,6 +56,8 @@ async function getPage(): Promise<Page | undefined> {
     title,
     content,
     metadata {
+      title,
+      description,
       'slug': slug.current,
     }
   }`;

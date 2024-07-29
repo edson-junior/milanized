1e49aeb40a4638de1f7595b88ab2d4907b943ab1
Edson Junior <soneddjr@gmail.com>
2024-07-29 16:31:39 +0200

    MIL-17: blogpost improvements
    Add essential metadata to blogposts:
    
    - _createdAt
    - _updatedAt
    - author
    - estimatedReadingTime
    
    author will become available on the design in the future

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -7,6 +7,8 @@ import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import client from '@/client';
 import Image from 'next/image';
 import { Blog } from '@/sanity.types';
+import { HTMLAttributes } from 'react';
+import { Clock2Icon } from 'lucide-react';
 
 interface BlogDetailsProps {
   params: { slug: string };
@@ -51,49 +53,88 @@ export async function generateMetadata({
 export default async function BlogDetails({ params }: BlogDetailsProps) {
   const data = await getPosts(params.slug);
 
-  if (data) {
-    return (
-      <div className="max-w-7xl mx-auto px-4 py-4">
-        <Heading as="h1" className="text-2xl lg:text-5xl mb-8">
-          {data.title}
-        </Heading>
-
-        {/* TODO: implement this */}
-        {/* <div className="flex gap-2 py-4">
-            <div>{data[0]?.attributes.category}</div>
-            <span>-</span>
-            <div>December 5, 2018</div>
-            <span>-</span>
-            <div>3 minute read</div>
-          </div> */}
-
-        {data?.featuredImage && (
-          <FeaturedImage featuredImage={data.featuredImage} />
-        )}
-
-        <main className="flex flex-col md:flex-row gap-8">
-          <article className="flex-1 md:w-64">
-            {data?.content && <BlockRendererClient value={data?.content} />}
-          </article>
-          {/* TODO: sidebar goes here */}
-        </main>
-      </div>
-    );
+  if (!data) {
+    return;
   }
+
+  const publishedAt = data ? new Date(data?._createdAt) : undefined;
+  const updatedAt = data ? new Date(data?._updatedAt) : undefined;
+
+  // TODO: create our own types files and not depend on typegen as much. Use typegen just to generate the files, then delete afterwards. Add it to the gitignore, even.
+
+  // author name will be re-enabled when author page is created
+  // const authorName = data?.author.name;
+  // @ts-expect-error: sanity's typegen doesn't create expanded refence types
+  const estimatedReadingTime = data.estimatedReadingTime;
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-4">
+      <div className="flex flex-col md:flex-row gap-8">
+        <div className="max-w-md">
+          <Heading as="h1" className="text-2xl lg:text-5xl mb-8">
+            {data.title}
+          </Heading>
+
+          <div className="flex gap-2 flex-col">
+            {/* {authorName && <p>{authorName}</p>} */}
+            {publishedAt && (
+              <>
+                <div>
+                  {new Intl.DateTimeFormat('en-GB', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                  }).format(publishedAt)}
+                </div>
+              </>
+            )}
+            {updatedAt && (
+              <div className="text-xs">
+                {'Last updated: '}
+                {new Intl.DateTimeFormat('en-GB', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                }).format(updatedAt)}
+              </div>
+            )}
+            {estimatedReadingTime && (
+              <div className="text-xs flex items-center gap-2">
+                <Clock2Icon className="w-4" />
+                {`${Math.ceil(estimatedReadingTime)} minute read`}
+              </div>
+            )}
+          </div>
+        </div>
+        {data?.featuredImage && (
+          <FeaturedImage
+            className="flex flex-col lg:shrink-0 grow"
+            featuredImage={data.featuredImage}
+          />
+        )}
+      </div>
+
+      <main className="flex flex-col md:flex-row gap-8">
+        <article className="flex-1 md:w-64">
+          {data?.content && <BlockRendererClient value={data?.content} />}
+        </article>
+      </main>
+    </div>
+  );
 }
 
 type FeaturedImageProps = {
   featuredImage: Blog['featuredImage'];
-};
+} & HTMLAttributes<HTMLElement>;
 
-function FeaturedImage({ featuredImage }: FeaturedImageProps) {
+function FeaturedImage({ featuredImage, ...props }: FeaturedImageProps) {
   if (!featuredImage) {
     return;
   }
 
   if (featuredImage.caption) {
     return (
-      <figure>
+      <figure {...props}>
         <Image
           width="1280"
           height="1280"
@@ -130,10 +171,15 @@ function urlFor(source: SanityImageSource) {
 async function getPosts(slug: string): Promise<Blog | undefined> {
   const query = `*[_type == 'blog' && metadata.slug.current == "${slug}"][0] {
     _id,
+    _createdAt,
+    _updatedAt,
     title,
     summary,
     content,
     featuredImage,
+    author->,
+    "authorImage": author->image,
+    "estimatedReadingTime": round(length(pt::text(content)) / 5 / 180),
     metadata {
       'slug': slug.current,
       slug,

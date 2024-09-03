23c0bd5a2a99c832570c53e9231f12cafbf42405
Edson Junior <soneddjr@gmail.com>
2024-09-03 14:09:42 +0200

    remove summary from sidebar, reduce image size

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,55 +14,51 @@ export default async function Sidebar({ slug }: SidebarProps) {
 
   return (
     <aside className="lg:w-96">
-      <Heading as="h2" className="text-xl lg:text-4xl py-2">
-        Latest Posts
-      </Heading>
+      <Heading className="block text-xl lg:text-4xl py-2">Latest Posts</Heading>
       <div className="flex flex-col gap-4 gap-y-6">
         {!posts?.length ? (
           <p>there are no blogposts</p>
         ) : (
-          posts?.map(
-            ({ _id, metadata, title, summary, featuredImage, _createdAt }) => {
-              const publishedAt = _createdAt ? new Date(_createdAt) : undefined;
+          posts?.map(({ _id, metadata, title, featuredImage, _createdAt }) => {
+            const publishedAt = _createdAt ? new Date(_createdAt) : undefined;
 
-              if (metadata?.slug === slug) {
-                return null;
-              }
-
-              return (
-                <Link href={`/${metadata?.slug}`} className="group" key={_id}>
-                  {featuredImage && (
-                    <Image
-                      width={250}
-                      height={250}
-                      loading="lazy"
-                      src={urlFor(featuredImage).width(600).url()}
-                      alt={featuredImage.alt || ''}
-                      className="w-full object-cover h-38"
-                    />
-                  )}
-
-                  <div className="py-4 lg:py-6">
-                    <Heading className="text-md lg:text-xl block mb-2 lg:mb-4 group-hover:text-blue-700">
-                      {title}
-                    </Heading>
-
-                    <p className="text text-sm line-clamp-4 align-baseline mb-2">
-                      {summary}
-                    </p>
-
-                    <p className="lg:hidden text text-xs line-clamp-4 align-baseline text-gray-600">
-                      {new Intl.DateTimeFormat('en-GB', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric'
-                      }).format(publishedAt)}
-                    </p>
-                  </div>
-                </Link>
-              );
+            if (metadata?.slug === slug) {
+              return null;
             }
-          )
+
+            return (
+              <Link
+                href={`/${metadata?.slug}`}
+                className="group flex flex-row gap-4"
+                key={_id}
+              >
+                {featuredImage && (
+                  <Image
+                    width={100}
+                    height={100}
+                    loading="lazy"
+                    src={urlFor(featuredImage).width(200).url()}
+                    alt={featuredImage.alt || ''}
+                    className="h-[revert-layer] object-cover"
+                  />
+                )}
+
+                <div>
+                  <Heading className="text-md block mb-2 group-hover:text-blue-700">
+                    {title}
+                  </Heading>
+
+                  <p className="text text-xs line-clamp-4 align-baseline text-gray-600">
+                    {new Intl.DateTimeFormat('en-GB', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric'
+                    }).format(publishedAt)}
+                  </p>
+                </div>
+              </Link>
+            );
+          })
         )}
       </div>
     </aside>

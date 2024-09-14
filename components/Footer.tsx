da09b6a229e9940de687afc05a15d2d42149ab27
Edson Junior <soneddjr@gmail.com>
2024-09-14 17:13:33 +0200

    fix footer padding issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,31 +17,33 @@ const links = [
 export default function Footer() {
   return (
     <footer className="bg-black text-white">
-      <div className="flex max-w-7xl mx-auto p-4">
-        <span className="font-bold uppercase text-sm mr-auto">
-          © {new Date().getFullYear()} Milanized!
-        </span>
-        <div className="flex ml-auto gap-4">
-          {links.map(({ href, text, icon }) => {
-            return (
-              <Link
-                className="text-2xl"
-                key={text}
-                href={href}
-                target="_blank"
-                aria-label={text}
-              >
-                {icon}
-              </Link>
-            );
-          })}
+      <div className="flex flex-col max-w-7xl mx-auto p-4">
+        <div className="flex pb-4">
+          <span className="font-bold uppercase text-sm mr-auto">
+            © {new Date().getFullYear()} Milanized!
+          </span>
+          <div className="flex ml-auto gap-4">
+            {links.map(({ href, text, icon }) => {
+              return (
+                <Link
+                  className="text-2xl"
+                  key={text}
+                  href={href}
+                  target="_blank"
+                  aria-label={text}
+                >
+                  {icon}
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className="flex max-w-7xl mx-auto py-6 px-4 border-t border-white">
-        <span className="mr-auto">{`Made with ❤️ in Milan`}</span>
-        <Link className="ml-auto underline" href="/privacy-policy">
-          privacy policy
-        </Link>
+        <div className="flex pt-4 border-t border-white">
+          <span className="mr-auto">{`Made with ❤️ in Milan`}</span>
+          <Link className="ml-auto underline" href="/privacy-policy">
+            privacy policy
+          </Link>
+        </div>
       </div>
     </footer>
   );

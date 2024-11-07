20da16c984fe17d0f48b12f7d817c83591d2652a
Edson Junior <soneddjr@gmail.com>
2024-11-07 15:31:08 +0100

    highlight table of contents titles

--- components/Toc.tsx
+++ components/Toc.tsx
@@ -2,6 +2,8 @@
 
 import { cn, slugify } from '@/lib/utils';
 import Heading from './ui/heading';
+import { useEffect } from 'react';
+import css from './Toc.module.css';
 
 export default function Toc({
   headings
@@ -11,16 +13,48 @@ export default function Toc({
     style: string;
   }[];
 }) {
+  useEffect(() => {
+    if (typeof document === 'undefined') return;
+
+    headings?.forEach(({ text }) => {
+      const target = document.getElementById(slugify(text));
+
+      if (!target) return;
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const tocItem = document.querySelector(
+            `[data-toc-item="${slugify(text)}"]`
+          );
+
+          if (entry.isIntersecting) {
+            tocItem?.classList.add(css.inView);
+          } else {
+            tocItem?.classList.remove(css.inView);
+          }
+        });
+      });
+
+      observer.observe(target);
+
+      return () => observer.disconnect();
+    });
+  }, [headings]);
+
   return (
     <div className="hidden lg:block mb-6">
       <Heading className="block text-lg">Table of contents</Heading>
 
       <ol className="anim-fade-to-b mt-2 leading-tight">
         {headings?.map(({ text, style }, index) => (
-          <li className="border-l transition-all text-sm" key={index}>
+          <li
+            className="border-l transition-all text-sm"
+            data-toc-item={slugify(text)}
+            key={index}
+          >
             <a
               className={cn(
-                'block p-1 border-l-transparent border-l-2 hover:border-l-black hover:bg-slate-100',
+                'block p-1 border-l-transparent border-l-2 hover:underline',
                 style == 'h2' && 'pl-4',
                 style == 'h3' && 'pl-6',
                 style == 'h4' && 'pl-8',

89110f8a3b3d3463f6096adf1412f8f5f7ca3b8c
Edson Junior <soneddjr@gmail.com>
2024-12-02 16:10:40 +0100

    enable code in portable text

--- components/BlockRenderClient.tsx
+++ components/BlockRenderClient.tsx
@@ -118,14 +118,14 @@ const components: PortableTextComponents = {
         </a>
       );
     }
-    // TODO: test this `code` mark
-    // code: ({ text }) => {
-    //   return <div>{parse(text)}</div>;
-    // }
   },
 
   block: {
-    normal: ({ children }) => {
+    normal: ({ children, value }) => {
+      if (value.children[0].marks.includes('code')) {
+        return <>{parse(value.children[0].text)}</>;
+      }
+
       return <p className="leading-7 [&:not(:last-child)]:mb-6">{children}</p>;
     },
     h1: ({ children }) => {

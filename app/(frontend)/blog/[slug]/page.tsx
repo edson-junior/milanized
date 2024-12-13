7efe5a0eb312d042798266260d4f2e95f9715d10
Edson Junior <soneddjr@gmail.com>
2024-12-13 10:40:44 +0100

    add support us banner on blogpost page

--- app/(frontend)/blog/[slug]/page.tsx
+++ app/(frontend)/blog/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { BlogPosting, WithContext } from 'schema-dts';
 import { getPostBySlug } from '@/sanity/lib/client';
 import { urlFor } from '@/sanity/lib/image';
 import Sidebar from '@/components/Sidebar';
+import { Button } from '@/components/ui/button';
 
 interface BlogDetailsProps {
   params: { slug: Slug };
@@ -170,6 +171,37 @@ export default async function BlogDetails({ params }: BlogDetailsProps) {
             )}
           </div>
           {data?.content && <BlockRendererClient value={data?.content} />}
+          <hr className="mb-8" />
+          <div className="max-w-screen-lg rounded border-0 border-l-4 border-spacing-2 border-green-700 bg-green-50 p-6 mb-6">
+            <div className="max-w-screen-sm">
+              <Heading
+                as="strong"
+                className="block text-lg lg:text-2xl py-0 lg:py-2 mb-2 scroll-m-20"
+              >
+                If you enjoyed our content, please consider supporting us!
+              </Heading>
+
+              <p>
+                With your support, we are able to continue creating
+                high-quality, informative content, plus new features and future
+                updates such that this website continues to grow.
+              </p>
+              <div className="flex flex-wrap items-center mt-8">
+                <Button
+                  asChild
+                  className="bg-green-700 action max-sm:w-full hover:bg-green-700/80"
+                >
+                  <a
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href="https://buymeacoffee.com/milanized"
+                  >
+                    Support us! ❤️
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </div>
         </article>
         <Sidebar slug={params.slug} headings={data.headings} />
       </main>

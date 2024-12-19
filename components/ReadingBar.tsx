b0cfd37af4d170d2476fa125314ee0681759d343
Edson Junior <soneddjr@gmail.com>
2024-12-19 16:20:37 +0100

    improve reading progress bar

--- components/ReadingBar.tsx
+++ components/ReadingBar.tsx
@@ -8,12 +8,14 @@ function ReadingBar() {
 
   if (currentRoute.includes('blog') && params?.slug !== undefined) {
     return (
-      <div
-        className="absolute top-full h-1 bg-red-700 w-full origin-[0_50%] animate-[scaleProgress_auto_linear]"
-        style={{
-          animationTimeline: 'scroll()'
-        }}
-      />
+      <div className="absolute top-full w-screen bg-red-300">
+        <div
+          className="h-1 bg-red-700 w-full origin-[0_50%] animate-[scaleProgress_auto_linear]"
+          style={{
+            animationTimeline: 'scroll()'
+          }}
+        />
+      </div>
     );
   }
 

58b93628d867c724d76e084ace6d89c8e1ad2f47
Edson Junior <soneddjr@gmail.com>
2025-06-09 16:53:50 +0200

    homepage new design

--- components/FeaturedPost.tsx
+++ components/FeaturedPost.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import Image from 'next/image';
 import Heading from '@/components/ui/heading';
-import { urlFor } from '@/sanity/lib/image';
-import { sanityFetch } from '@/sanity/lib/client';
-import groq from 'groq';
-import { Blog } from '@/sanity.types';
 import { formatDate } from '@/lib/utils';
+import { Blog } from '@/sanity.types';
+import { sanityFetch } from '@/sanity/lib/client';
+import { urlFor } from '@/sanity/lib/image';
+import groq from 'groq';
+import Image from 'next/image';
+import Link from 'next/link';
 const query = groq`*[_type == 'blog' && !(_id in path('drafts.**')) && isFeatured]|order(_createdAt desc) [0...1] [0] {
   _id,
   _createdAt,
@@ -43,36 +43,43 @@ export default async function FeaturedPost() {
   const publishedAt = new Date(publishDate || _createdAt);
 
   return (
-    <Link
-      href={`/blog/${metadata?.slug}`}
-      className="mb-12 relative group grid items-center gap-x-8 gap-y-4 md:grid-cols-2"
-    >
-      {featuredImage && (
-        <figure className="max-md:full-bleed relative aspect-video overflow-hidden rounded-md">
-          <Image
-            fill
-            sizes="(min-width: 1360px) 608px, (min-width: 780px) 44.64vw, calc(100vw - 32px)"
-            src={urlFor(featuredImage).width(800).url()}
-            alt={featuredImage.alt || ''}
-            priority
-            className="object-cover transition duration-200 ease-in-out transform group-hover:scale-110"
-            blurDataURL={featuredImage.lqip}
-            placeholder="blur"
-          />
-        </figure>
-      )}
+    <section className="bg-black text-white">
+      <div className="max-w-7xl mx-auto px-4 py-16 lg:py-20">
+        <Link
+          href={`/blog/${metadata?.slug}`}
+          className="relative group grid items-center gap-8 md:grid-cols-2"
+        >
+          <div className="max-w-lg space-y-4">
+            <Heading className="text-xl lg:text-3xl block mb-4 bg-transparent lg:p-2 lg:-ml-2 lg:group-hover:text-black lg:group-hover:bg-white">
+              {title}
+            </Heading>
+            <p className="text-sm lg:text-lg line-clamp-4 align-baseline">
+              {summary}
+            </p>
+            <p className="text-sm">
+              <span className="text-gray-600">{formatDate(publishedAt)}</span>
+            </p>
+          </div>
 
-      <div className="max-w-lg space-y-4">
-        <Heading className="text-md lg:text-4xl block mb-4 group-hover:text-blue-700">
-          {title}
-        </Heading>
-        <p className="text-sm lg:text-lg line-clamp-4 align-baseline">
-          {summary}
-        </p>
-        <p className="text-sm">
-          <span className="text-gray-600">{formatDate(publishedAt)}</span>
-        </p>
+          {featuredImage && (
+            <figure className="max-md:full-bleed relative aspect-video overflow-hidden rounded-md -order-1 lg:order-none">
+              <Image
+                fill
+                sizes="(min-width: 1360px) 608px, (min-width: 780px) 44.64vw, calc(100vw - 32px)"
+                src={urlFor(featuredImage).width(800).url()}
+                alt={featuredImage.alt || ''}
+                priority
+                className="object-cover transition duration-200 ease-in-out transform group-hover:scale-105"
+                blurDataURL={featuredImage.lqip}
+                placeholder="blur"
+              />
+              <span className="absolute text-white bg-black/70 top-0 right-4 rounded-t-none leading-7 px-4 rounded-br-sm rounded-bl-sm text-xs">
+                Featured
+              </span>
+            </figure>
+          )}
+        </Link>
       </div>
-    </Link>
+    </section>
   );
 }

56f18a6be71f16072477fca4c247daf9eb2da8f7
Edson Junior <soneddjr@gmail.com>
2025-07-10 00:04:25 +0200

    disable cloudinary

--- components/FeaturedImage.tsx
+++ components/FeaturedImage.tsx
@@ -1,10 +1,6 @@
-'use client';
-
-import { createImageUrl } from '@/lib/utils';
 import { Blog } from '@/sanity.types';
 import { urlFor } from '@/sanity/lib/image';
 import parse from 'html-react-parser';
-import { CldImage } from 'next-cloudinary';
 import Image from 'next/image';
 import { HTMLAttributes } from 'react';
 
@@ -22,50 +18,20 @@ export function FeaturedImage({
     return;
   }
 
-  let imgUrl;
-
-  if (image?.public_id) {
-    imgUrl = createImageUrl(image.public_id, [
-      {
-        quality: 'auto',
-        format: 'auto'
-      },
-      {
-        width: 900,
-        aspect_ratio: '16:9',
-        crop: 'fill'
-      }
-    ]);
-  }
-
   if (featuredImage.caption) {
     return (
       <figure {...props}>
         <div className="w-full aspect-video relative mb-2 overflow-hidden rounded-md">
-          {image?.public_id ? (
-            <Image
-              priority
-              width={image.width}
-              height={image.height}
-              // sizes="(min-width: 1340px) 832px, (min-width: 1040px) calc(85.71vw - 299px), 50vw"
-              src={`${imgUrl}`}
-              alt={featuredImage.alt || ''}
-              blurDataURL={featuredImage.lqip}
-              placeholder="blur"
-              className="object-cover transition duration-200 ease-in-out transform hover:scale-110"
-            />
-          ) : (
-            <Image
-              fill
-              priority
-              sizes="(min-width: 1340px) 832px, (min-width: 1040px) calc(85.71vw - 299px), 50vw"
-              src={urlFor(featuredImage).width(1280).url()}
-              alt={featuredImage.alt || ''}
-              blurDataURL={featuredImage.lqip}
-              placeholder="blur"
-              className="object-cover transition duration-200 ease-in-out transform hover:scale-110"
-            />
-          )}
+          <Image
+            fill
+            priority
+            sizes="(min-width: 1340px) 832px, (min-width: 1040px) calc(85.71vw - 299px), 50vw"
+            src={urlFor(featuredImage).width(900).url()}
+            alt={featuredImage.alt || ''}
+            blurDataURL={featuredImage.lqip}
+            placeholder="blur"
+            className="object-cover transition duration-200 ease-in-out transform hover:scale-110"
+          />
         </div>
         <figcaption className="text-xs italic text-gray-600 [&>a]:text-blue-700 [&>a]:underline mb-4">
           {parse(featuredImage?.caption)}

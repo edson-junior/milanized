c87797f05273a364e97a7fb4f9c1cd36b77d3bf9
Edson Junior <soneddjr@gmail.com>
2025-08-18 15:34:24 +0200

    remove unnecessary redirects

--- next.config.mjs
+++ next.config.mjs
@@ -29,37 +29,6 @@ const nextConfig = {
       };
     }
     return config;
-  },
-  async redirects() {
-    return [
-      {
-        source:
-          '/blog/essential-tips-for-moving-to-italy-things-to-know-before-moving',
-        destination: '/blog/how-to-move-to-italy',
-        permanent: true
-      },
-      {
-        source: '/blog/the-pros-and-cons-of-having-a-car-in-milan',
-        destination: '/blog/driving-in-milan',
-        permanent: true
-      },
-      {
-        source: '/blog/discovering-italy-10-unforgettable-day-trips-from-milan',
-        destination: '/blog/day-trips-from-milan',
-        permanent: true
-      },
-      {
-        source: '/author/milanized-crew',
-        destination: '/author/milanized',
-        permanent: true
-      },
-      {
-        source:
-          '/blog/escape-the-city-10-incredible-city-escape-destinations-to-explore-from-milan',
-        destination: '/blog/best-trips-from-milan',
-        permanent: true
-      }
-    ];
   }
 };
 

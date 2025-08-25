7ac2b598d3de9addc0f993f999ceaac3ea3f4bc3
Edson Junior <soneddjr@gmail.com>
2025-08-25 11:10:42 +0200

    prevent next.js from storing fetch cache
    next.js allocates a maximum of 2mb of storeable cache, since we
    surpassed that, it's necessary to disable the cache.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -36,9 +36,10 @@ export async function sanityFetch<const QueryString extends string>({
 }) {
   return client.fetch(query, params, {
     next: {
-      revalidate: tags.length ? false : revalidate, // for simple, time-based revalidation
+      // revalidate: tags.length ? false : revalidate, // for simple, time-based revalidation
       tags // for tag-based revalidation
-    }
+    },
+    cache: 'no-store'
   });
 }
 

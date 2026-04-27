aff329a78b6ccc894f6e98ede56bb39294f5297a
Edson Junior <soneddjr@gmail.com>
2026-04-27 19:51:22 +0200

    add critical css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 import type { Viewport } from 'next';
 import { Catamaran, Open_Sans } from 'next/font/google';
-import './../styles/globals.css';
+import '../styles/globals.css';
 import { GoogleTagManager } from '@next/third-parties/google';
+import parse from 'html-react-parser';
 import NextTopLoader from 'nextjs-toploader';
 import Footer from './../components/Footer';
 import Header from './../components/Header/Header';
+
+const criticalCss = readFileSync(
+  join(process.cwd(), 'styles/critical.css'),
+  'utf-8'
+);
 
 const openSans = Open_Sans({ subsets: ['latin'], variable: '--open-sans' });
 const catamaran = Catamaran({ subsets: ['latin'], variable: '--catamaran' });
@@ -20,6 +28,7 @@ type RootLayoutProps = Readonly<{
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en-GB" className="lg:scroll-smooth" suppressHydrationWarning>
+      <head>{parse(`<style>${criticalCss}</style>`)}</head>
       <body
         className={`${openSans.variable} ${catamaran.variable} font-sans bg-soft-white`}
       >
